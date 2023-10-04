797e1a6e095a795a15951ab842fc75765b7baeaa
victoryoung <vicmachine08@gmail.com>
2023-10-04 19:22:45 +1100

    basic layout with imgs

--- exercise-habits/src/App.jsx
+++ exercise-habits/src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <Header />
+      <Logos />
+      <Feature
+        h3={
+          "Give your body what it needs to build strength, confidence and fitness"
+        }
+        p={
+          "Exercise is an amazing way to keep your body strong, flexible and healthy"
+        }
+        imgSrc={""}
+      />
+      <Form />
     </>
-  )
+  );
 }
 
-export default App
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <img src="" alt="" />
+    </nav>
+  );
+}
+
+function Header() {
+  return (
+    <header className="container-l flex-row">
+      <div className="card">
+        <h1>Exercise Physiology for NDIS and HomeCare</h1>
+        <p>Use exercise to improve your health, confidence and how you feel</p>
+        <button className="primary-btn">Get Started</button>
+      </div>
+      <div className="card">
+        <img src="" alt="" />
+      </div>
+    </header>
+  );
+}
+
+function Logos() {
+  return (
+    <section className="container-l flex-col">
+      <h3 className="text-align-center">Trusted by</h3>
+      <div className="logo-container">
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+    </section>
+  );
+}
+
+function Feature({ h3, p, imgSrc }) {
+  return (
+    <div className="container-l flex-row">
+      <div className="card">
+        <h3>{h3}</h3>
+        <p>{p}</p>
+      </div>
+      <div className="card">
+        <img src={imgSrc} alt="" />
+      </div>
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <div className="container-l">
+      <form action="submit" className="form main">
+        <label htmlFor="FNAME">First Name</label>
+        <input type="text" />
+        <label htmlFor="LNAME">Last Name</label>
+        <input type="text" />
+        <label htmlFor="PHONE">Your Best Contact Nubmer</label>
+        <input type="phone" />
+        <label htmlFor="EMAIL">Your Email</label>
+        <input type="email" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
